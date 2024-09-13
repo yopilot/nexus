b57c1289666e2cf4808c42bf738c29da5e4e205d
Yogesh Sharma <141168483+yopilot@users.noreply.github.com>
2024-09-13 21:54:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.mjs', // or 'postcss.config.cjs' if you chose Option 2
-  },
-});
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000
+  }
+})
